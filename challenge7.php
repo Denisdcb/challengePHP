c25c9581fbc0dce4cf3cb5a0ca942cc7875040bd
Denisdcb <denis.dcbr@gmail.com>
2024-10-16 15:37:04 +0200

    Challenge-7

---
+++ challenge7.php
@@ -0,0 +1,20 @@
+<?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Denis" => [
+        "city" => "Sens",
+        "weapon" => "Arc"
+    ]
+];
+var_dump($characters);
